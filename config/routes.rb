b972956892329cf534b4b85b1aa6c48ea7f08ff9
Hugo MANCINI <hgmancini@gmail.com>
2019-08-14 15:00:29 +0200

    exo 2 ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :restaurants do
+    resources :reviews, only: [:new, :create, :index]
+  end
+  resources :reviews, only: [:edit, :show, :destroy, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Restaurant.destroy_all
+
+10.times do
+  Restaurant.create(name: Faker::Cannabis.buzzword, address: Faker::Address.street_name, phone_number: Faker::PhoneNumber.phone_number, category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+end
+
